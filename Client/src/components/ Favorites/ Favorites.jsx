b1ec74c4a9dbc7f486a51a16943d46844f107436
juf468 <julia.franchi7@gmail.com>
2024-09-30 15:45:19 -0300

    fix

--- Client/src/components/ Favorites/ Favorites.jsx
+++ Client/src/components/ Favorites/ Favorites.jsx
@@ -3,7 +3,6 @@ import Card from '../card/Card';
 import { filterFavorites, orderFavorites, reset } from '../../Redux/actions';
 import { useState } from 'react';
 
-
 const Favorites = () => {
 	const favorites = useSelector((state) => state.myFavorites);
 	const [aux, setAux] = useState(false);
@@ -16,24 +15,26 @@ const Favorites = () => {
 
 		dispatch(orderFavorites(value));
 	};
+
 	const handleFilter = (event) => {
 		event.preventDefault();
 		const { value } = event.target;
 
 		dispatch(filterFavorites(value));
 	};
+
 	const resetButton = () => {
 		dispatch(reset());
 	};
 
 	return (
-		<div className="max-h-screen flex flex-col">
-			<div className=" flex justify-center mb-4 top-10 mt-20">
+		<div className="flex flex-col items-center justify-center bg-transparent w-full absolute top-40">
+			<div className="flex justify-center mt-10 flex-row mx-auto space-x-4">
 				<select
 					onChange={handleOrder}
 					name="order"
 					defaultValue={'DEFAULT'}
-					className='cursor-pointer text-xl font-bold bg-colorButtonLogin text-black border-4 border-solid border-black text-md rounded-xl p-6 mx-2 focus:outline-none'
+					className='cursor-pointer text-xl font-bold bg-colorButtonLogin text-black border-4 border-solid border-black text-md rounded-xl p-6 focus:outline-none'
 				>
 					<option value="DEFAULT" disabled>
 						Select Order
@@ -45,7 +46,7 @@ const Favorites = () => {
 					onChange={handleFilter}
 					name="filter"
 					defaultValue={'DEFAULT'}
-					className='cursor-pointer text-xl font-bold bg-colorButtonLogin text-black border-4 border-solid border-black text-md rounded-xl p-6 mx-2 focus:outline-none'
+					className='cursor-pointer text-xl font-bold bg-colorButtonLogin text-black border-4 border-solid border-black text-md rounded-xl p-6 focus:outline-none'
 				>
 					<option value="DEFAULT" disabled>
 						Select filter
@@ -56,11 +57,15 @@ const Favorites = () => {
 					<option value="unknow">unknow</option>
 				</select>
 
-				<button onClick={resetButton} className='min-w-[20] mx-2 p-6 border-solid border-colorBorderForm border-4 rounded-xl underline-none text-white bg-transparent font-bold text-xl cursor-pointer hover:border-white transition-transform ease-in-out duration-200'>
+				<button
+					onClick={resetButton}
+					className='min-w-[20] p-6 border-solid border-colorBorderForm border-4 rounded-xl text-white bg-transparent font-bold text-xl cursor-pointer hover:border-white transition-transform ease-in-out duration-200'
+				>
 					RESET
 				</button>
 			</div>
-			<div className='flex flex-wrap'>
+
+			<div className='flex flex-wrap justify-center items-center w-full mt-8'>
 				{favorites?.map(
 					({ id, name, species, gender, origin, status, image }) => (
 						<Card
