f09b92143fec1c34cf4b4921217941a1f7d93d9e
juf468 <julia.franchi7@gmail.com>
2024-10-04 20:51:24 -0300

    axios services in App

--- Client/src/Services/Get/getUser.ts
+++ Client/src/Services/Get/getUser.ts
@@ -1,18 +1,13 @@
 // authService.ts
 import api from '../Axios'; 
 
+
+
 export interface LoginResponse {
+  access:boolean
+ }
  
-  token: string; 
-  user: {
-      id: string;
-      email: string;
-      // Otros campos que necesites
-  };
-}
 export const loginUser = async (username: string, password: string): Promise<LoginResponse> => {
- 
         const { data } = await api.get<LoginResponse>(`/rickandmorty/login/?email=${username}&password=${password}`);
-        return data; // Devuelve los datos recibidos
-  
+        return data; 
 };
